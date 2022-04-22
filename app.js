9b00383035fb89756fb2d665f1eb321c6855476f
Ayaz Ahmad <forgithub44@gmail.com>
2022-04-22 23:36:41 +0500

    Second Last Challenge Complete

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const _ = require('lodash')
 const ejs = require("ejs")
 const { process_params } = require("express/lib/router")
 const { application } = require("express")
+const { toUpper, upperCase } = require("lodash")
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
@@ -51,25 +52,25 @@ app.route('/compose')
 
   })
 
-// :POSTS
-app.route('/posts/:userId')
-  .get((req, res) => {
-    let userid = _.lowerCase(req.params.userId)
-    post.forEach((i) => {
-      const storedTitle = _.lowerCase(i.composeTitle)
 
-      if (storedTitle === userid) {
-        console.log('Match Found')
-      } else {
-        // console.log(req.params.userId)
-        // console.log(storedTitle)
-      }
-    })
+// :POST
+app.get('/posts/:userId', (req, res) => {
+  let userid = _.lowerCase(req.params.userId)
+  post.forEach((i) => {
+    const storedTitle = _.lowerCase(i.composeTitle)
 
+    if (storedTitle == userid) {
+      console.log('Match Found')
+      res.render('post', { list: _.upperFirst(storedTitle), listPost: i.composePost })
+    } else {
+      console.log('Match not found')
+    }
   })
+})
 
 
 // :LISTEN
-app.listen(3000, function () {
-  console.log("Server started on port 3000")
+app.listen(3000 || process.env.PORT, () => {
+  console.log('Listen at the port 3000')
 })
+
